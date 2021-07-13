74edf3de27986744eeb668ba3ade629a855d8478
Craig Morten <cmorten@users.noreply.github.com>
2021-07-13 13:59:42 +0100

    feat: add benchmarks (#1)

---
+++ benchmark/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo
+deno run --allow-net --unstable --no-check --reload $1 'localhost:4505' &
+pid=$!
+
+while [[ "$(curl -s -o /dev/null -I -w '%{http_code}' 'http://localhost:4505/')" != "200" ]]; do
+  sleep 5;
+done
+
+wrk 'http://localhost:4505/' -d '20s' --latency
+
+kill $pid
