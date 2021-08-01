000bac8b4b69be5fd8df189328c565b8650c0373
cmorten <craig.morten@hotmail.co.uk>
2021-08-01 12:00:09 +0100

    feat: 0.4.0

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { MuxAsyncIterator } from "https://deno.land/std@0.101.0/async/mod.ts";
+export { MuxAsyncIterator } from "https://deno.land/std@0.103.0/async/mod.ts";
--- test/deps.ts
+++ test/deps.ts
@@ -1,15 +1,15 @@
-export { readAll, writeAll } from "https://deno.land/std@0.101.0/io/util.ts";
+export { readAll, writeAll } from "https://deno.land/std@0.103.0/io/util.ts";
 export {
   assert,
   assertThrowsAsync,
   equal,
   unreachable,
-} from "https://deno.land/std@0.101.0/testing/asserts.ts";
+} from "https://deno.land/std@0.103.0/testing/asserts.ts";
 export {
   dirname,
   fromFileUrl,
   resolve,
-} from "https://deno.land/std@0.101.0/path/mod.ts";
-export { deferred } from "https://deno.land/std@0.101.0/async/mod.ts";
-export type { Deferred } from "https://deno.land/std@0.101.0/async/mod.ts";
-export { superdeno } from "https://deno.land/x/superdeno@4.3.0/mod.ts";
+} from "https://deno.land/std@0.103.0/path/mod.ts";
+export { deferred } from "https://deno.land/std@0.103.0/async/mod.ts";
+export type { Deferred } from "https://deno.land/std@0.103.0/async/mod.ts";
+export { superdeno } from "https://deno.land/x/superdeno@4.4.0/mod.ts";
